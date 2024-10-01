c79c0133787afc4f2d4ec8b6db321288f3973b1b
Md. Arif <arifmia1129@gmail.com>
2024-10-01 16:26:34 +0600

    Route, View
    Route: Actually, it is the path of the project. After typing the route name with the URL, we can go to our expected page. There are many types of routes type: Get, Post, Delete, Put, etc. We can use them when we need those types of work.
    
    View: The view file format will be blade.php. Here, we can use HTML and other things. We use the view to show route content. We can return a string and inline HTML as the return of the route.

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+    <ul>
+        <li>Name: Arif</li>
+        <li>Designation: Software Developer</li>
+        <li>Address: Dhaka, Bangladesh</li>
+    </ul>
+</body>
+</html>
