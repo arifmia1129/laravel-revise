227630dc6a72c13db3ba60cbf28853ce5d27a871
Md. Arif <arifmia1129@gmail.com>
2024-10-06 08:12:07 +0600

    Blade Directive - Loop

--- resources/views/raw_php.blade.php
+++ resources/views/raw_php.blade.php
@@ -1,7 +1,44 @@
-@php
+{{-- @php
     $name =[
     'Arif', 'Binu', 'Ariba'
 ];
 @endphp
 
-{{$name[0]}}
+{{$name[0]}} --}}
+
+@php
+    $names = [
+        'Arif',
+        'Binu',
+        'Ariba',
+        'Urmi',
+        'Hannan',
+        'Sadik',
+        'Soikot'
+    ];
+
+    $ages=[]
+@endphp
+
+@for ($i = 1; $i <= 10; $i++) 
+    @for ($j = 1; $j <= 10; $j++)
+        <p>{{ $i }} x {{ $j }} = {{ $i * $j }}</p>
+        @if ($i == 1 && $j == 10)
+            @break(2) 
+        @endif
+    @endfor
+    <br>
+    <br>
+@endfor
+
+
+@foreach ($names as $name)
+    <p>{{$name}}</p>
+@endforeach
+
+@forelse ($ages as $age)
+    <p>{{$age}}</p>
+@empty
+    <p>Age not found</p>
+@endforelse
+
