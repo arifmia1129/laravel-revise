a6c1c7c9be23f5359176f790d6840f2094428604
Md. Arif <arifmia1129@gmail.com>
2024-10-09 22:17:32 +0600

    Global middleware

---
+++ app/Http/Middleware/GlobalMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class GlobalMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $current_year = date('Y');
+
+        if($current_year > 2020) {
+            echo 'Application is closing';
+            exit;
+        }
+
+        return $next($request);
+    }
+}
