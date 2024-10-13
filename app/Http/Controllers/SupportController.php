212f62f0110c3cad136ab2a6d730a18691e2d0bf
Md. Arif <arifmia1129@gmail.com>
2024-10-13 21:38:46 +0600

    Form Validation - Regular Expression Validation

--- app/Http/Controllers/SupportController.php
+++ app/Http/Controllers/SupportController.php
@@ -19,7 +19,7 @@ class SupportController extends Controller
         'email'=> 'required|email',
         'message' => 'required',
         'serial'=>'required|numeric',
-        'date'=>'required|date'
+        'date'=>['required', 'regex:/^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\d{4})$/']
        ]); 
 
       return  response()->json([
