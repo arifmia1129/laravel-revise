0175e58090932951c38dd22ca66f0835e0c5d4f9
Md. Arif <arifmia1129@gmail.com>
2024-10-14 15:57:55 +0600

    Form Validation - Customizing Error Messages

--- app/Http/Controllers/SupportController.php
+++ app/Http/Controllers/SupportController.php
@@ -16,13 +16,17 @@ class SupportController extends Controller
         // dd($request->all());
 
        $request->validate([
-        'name' => 'required|string|min:3|max:20',
+        'name' => 'required|min:3|max:20',
         'email'=> 'required|email',
         'password'=> ['required', Password::min(8)->mixedCase() ],
         'confirm_password'=> ['required', 'same:password'],
         'message' => 'required',
         'serial'=>'required|numeric',
         'date'=>['required', 'regex:/^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\d{4})$/']
+       ], [
+        'name.min'=>'Need to be your full name'
+       ], [
+        'name'=>'full name'
        ]); 
 
       return  response()->json([
