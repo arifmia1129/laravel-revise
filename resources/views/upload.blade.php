6b72ac624764d08d28a5be24d68d951191ead4d1
Md. Arif <arifmia1129@gmail.com>
2024-10-16 19:28:52 +0600

    File Uploading (Storage Link)

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,3 +1,8 @@
+<div>
+    <h1>Welcome to Laravel image upload system</h1>
+    <img style="width:300px;" src="{{asset('storage/uploads/new_file.png')}}" alt="image">
+</div>
+
 <form action="{{route('upload_store')}}" method="post" enctype="multipart/form-data">
     @csrf
 <input type="file" name="file" >
