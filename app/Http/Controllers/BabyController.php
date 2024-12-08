348370b7a506900b0aaec334de2cc2517a104ca2
Md. Arif <arifmia1129@gmail.com>
2024-12-08 18:11:14 +0600

    Eloquent Relationship - One to One - Inverse

--- app/Http/Controllers/BabyController.php
+++ app/Http/Controllers/BabyController.php
@@ -3,19 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Baby;
+use App\Models\BabyProfile;
 use Illuminate\Http\Request;
 
 class BabyController extends Controller
 {
     public function index () {
-        $all_babies = Baby::with('profile')->get();
+    //     $all_babies = Baby::with('profile')->get();
 
-        
+    //   return  response()->json([
+    //     'success'=> true,
+    //     'message'=>'Successfully retrieved all babies information.',
+    //     'data' => $all_babies
+    //   ]);
 
-      return  response()->json([
-        'success'=> true,
-        'message'=>'Successfully retrieved all babies information.',
-        'data' => $all_babies
-      ]);
+    $all_profile = BabyProfile::with('baby')->get();
+
+    return  response()->json([
+      'success'=> true,
+      'message'=>'Successfully retrieved all baby profiles.',
+      'data' => $all_profile
+    ]);
     }
 }
